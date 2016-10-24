969e01cf4d2ac14a825e88f9b01090cdf2e6818d
Kim Songhyun <yeonmu4121@gmail.com>
2016-10-24 12:35:22 +0900

    Implement toggle_graph procedure

--- sql/graph.sql
+++ sql/graph.sql
@@ -1,6 +1,7 @@
 DROP PROCEDURE IF EXISTS idb.create_graph;
 DROP PROCEDURE IF EXISTS idb.change_graph_name;
 DROP PROCEDURE IF EXISTS idb.delete_graph;
+DROP PROCEDURE IF EXISTS idb.toggle_graph;
 
 DELIMITER $$
 
@@ -17,6 +18,12 @@ END$$
 CREATE PROCEDURE idb.delete_graph(id INTEGER)
 BEGIN
     DELETE FROM graph WHERE graph.id=id;
+END$$
+
+CREATE PROCEDURE idb.toggle_graph(id INTEGER)
+BEGIN
+    UPDATE graph SET graph.duration=IF(graph.activated=1, NULL, 20) WHERE graph.id=id;
+    UPDATE graph SET graph.activated=IF(graph.activated=1, 0, 1) WHERE graph.id=id;
 END$$
 
 DELIMITER ;
