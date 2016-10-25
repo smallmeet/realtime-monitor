092d3fa5d0ba5bf01fbc2ba5dddbbdc8d082eeb8
Kim Songhyun <yeonmu4121@gmail.com>
2016-10-25 16:53:00 +0900

    JSON page design
    /json is implemented.
    This returns the json string about the data of graph which is activated.

--- app.py
+++ app.py
@@ -31,7 +31,42 @@ def insert(deviceId, data):
 
 @app.route('/json')
 def json():
-    return 'json'
+    cur = conn.getCursor()
+    cur.execute('SELECT `id`, `duration`, `from`, `to`, `order` FROM graph WHERE `activated`=1')
+    graphes = []
+    for row in cur:
+        graphes.append(row)
+
+    result = {}
+    for graph in graphes:
+        graphId = 'g' + str(graph[0])
+
+        if graphId not in result:
+            result[graphId] = {}
+            result[graphId]['order'] = graph[4]
+            result[graphId]['devices'] = {}
+
+        if graph[1] is None:
+            if graph[3] is None: # from
+                query = 'CALL get_data_from({graph_id}, {f})'.format(graph_id=graph[0], f=graph[2])
+            else: # to
+                query = 'CALL get_data_from_to({graph_id}, {f}, {t})'.format(graph_id=graph[0], f=graph[2], t=graph[3])
+        else: # realtime
+            query = 'CALL get_data_in_realtime({graph_id}, {duration})'.format(graph_id=graph[0], duration=graph[1])
+
+        cur.execute(query) # device_id, label_id, value, updated
+        for row in cur:
+            deviceId = 'd' + str(row[0])
+            labelId = 'l' + str(row[1])
+
+            if deviceId not in result[graphId]['devices']:
+                result[graphId]['devices'][deviceId] = {}
+            if labelId not in result[graphId]['devices'][deviceId]:
+                result[graphId]['devices'][deviceId][labelId] = []
+            result[graphId]['devices'][deviceId][labelId].append({'value':row[2], 'updated':str(row[3])})
+
+    cur.close()
+    return str(result).replace('\'', '\"')
 
 @app.route('/static/<path:filename>')
 def loadStatic(filename):
