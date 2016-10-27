c0f44d719f1cc8c5f64f0e8bf4d4b6bfca74849d
Kim Songhyun <yeonmu4121@gmail.com>
2016-10-27 19:25:37 +0900

    Implement getter methods for Graph class

--- static/js/graph/graph.js
+++ static/js/graph/graph.js
@@ -1,11 +1,23 @@
-function Graph(graphId, data) {
-    this._graphId = graphId;
+function Graph(id, data) {
+    this._id = id;
     this._order = data.order;
     this._data = data.devices;
 }
 
+Graph.prototype.getId = function() {
+    return this._id;
+}
+
+Graph.prototype.getOrder = function() {
+    return this._order;
+}
+
+Graph.prototype.getData = function() {
+    return this._data;
+}
+
 Graph.prototype.drawGraph = function() {
-    console.log("There's no proper function for this graph.");
+    console.log("There's no proper function for " + this._id);
 }
 
 Graph.prototype.update = function(data) {
