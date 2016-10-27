851e54098c4bdc326c0e2f60ed516e218d4b27f2
Kim Songhyun <yeonmu4121@gmail.com>
2016-10-27 18:10:50 +0900

    Implement graph structure
    - Graph.drawGraph():
    Draw a graph according to the data it has.
    
    - graphList.getGraph(graphId):
    Return graph object by its id.

--- static/js/graph/graph.js
+++ static/js/graph/graph.js
@@ -3,3 +3,12 @@ function Graph(graphId, data) {
     this._order = data.order;
     this._data = data.devices;
 }
+
+Graph.prototype.drawGraph = function() {
+    console.log("There's no proper function for this graph.");
+}
+
+Graph.prototype.update = function(data) {
+    this._order = data.order;
+    this._data = data.devices;
+}
--- static/js/graph/graph_list.js
+++ static/js/graph/graph_list.js
@@ -7,14 +7,22 @@ graphList = new function() {
                 type: 'GET',
                 url: '/json',
                 success: function(result) {
-                    graphList._graphes = {};
                     result = JSON.parse(result);
                     keys = Object.keys(result);
                     for(i=0; i<keys.length; i++) {
-                        graphList._graphes[keys[i]] = new Graph(keys[i], result[keys[i]]);
+                        if(keys[i] in graphList._graphes) {
+                            graphList._graphes[keys[i]].update(result[keys[i]]);
+                        }
+                        else {
+                            graphList._graphes[keys[i]] = new Graph(keys[i], result[keys[i]]);
+                        }
                     }
                 },
             });
         });
+    }
+
+    this.getGraph = function(graphId) {
+        return this._graphes[graphId];
     }
 }
