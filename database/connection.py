bd4d7815392b0d68c38829aa0373645accf9a65e
Kim Songhyun <yeonmu4121@gmail.com>
2016-10-27 21:22:52 +0900

    Add private variables to Connection class
    The parameters being passed to self._conn are now private variables.

--- database/connection.py
+++ database/connection.py
@@ -2,7 +2,13 @@ import pymysql
 
 class Connection:
     def __init__(self, config):
-        self._conn = pymysql.connect(host=config['host'], port=int(config['port']), user=config['user'], passwd=config['passwd'], db=config['db'], charset='utf8')
+        self._host = config['host']
+        self._port = int(config['port'])
+        self._user = config['user']
+        self._passwd = config['passwd']
+        self._db = config['db']
+        self._charset = 'utf8'
+        self._conn = pymysql.connect(host=self._host, port=self._port, user=self._user, passwd=self._passwd, db=self._db, charset=self._charset)
 
     def getCursor(self):
         return self._conn.cursor()
