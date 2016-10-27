f8b2ce30177df54880efa714bb4256533c94a271
Kim Songhyun <yeonmu4121@gmail.com>
2016-10-27 18:31:33 +0900

    Implement graphList.updateData()
    graphList.updateData():
    This is AJAX success function.
    It is implemented for encapsulation.

--- static/js/graph/graph_list.js
+++ static/js/graph/graph_list.js
@@ -7,22 +7,26 @@ graphList = new function() {
                 type: 'GET',
                 url: '/json',
                 success: function(result) {
-                    result = JSON.parse(result);
-                    keys = Object.keys(result);
-                    for(i=0; i<keys.length; i++) {
-                        if(keys[i] in graphList._graphes) {
-                            graphList._graphes[keys[i]].update(result[keys[i]]);
-                        }
-                        else {
-                            graphList._graphes[keys[i]] = new Graph(keys[i], result[keys[i]]);
-                        }
-                    }
-                },
+                    graphList.updateData(result)
+                }
             });
         });
     }
 
     this.getGraph = function(graphId) {
         return this._graphes[graphId];
+    }
+
+    this.updateData = function(result) {
+        result = JSON.parse(result);
+        keys = Object.keys(result);
+        for(i=0; i<keys.length; i++) {
+            if(keys[i] in graphList._graphes) {
+                this._graphes[keys[i]].update(result[keys[i]]);
+            }
+            else {
+                this._graphes[keys[i]] = new Graph(keys[i], result[keys[i]]);
+            }
+        }
     }
 }
