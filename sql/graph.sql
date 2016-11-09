274f8050978f4770892d72e9c713dfbf53d7ccbd
Kim Songhyun <yeonmu4121@gmail.com>
2016-11-10 02:01:30 +0900

    Implement setter procedure
    - set_duration(graph_id, duration)
    Set the duration of graph of which id is graph_id.
    
    - set_start_and_finish(graph_id, start, finish)
    Set start and end field.

--- sql/graph.sql
+++ sql/graph.sql
@@ -2,6 +2,8 @@ DROP PROCEDURE IF EXISTS idb.create_graph;
 DROP PROCEDURE IF EXISTS idb.change_graph_name;
 DROP PROCEDURE IF EXISTS idb.delete_graph;
 DROP PROCEDURE IF EXISTS idb.toggle_graph;
+DROP PROCEDURE IF EXISTS idb.set_duration;
+DROP PROCEDURE IF EXISTS idb.set_start_and_finish;
 
 DELIMITER $$
 
@@ -30,6 +32,20 @@ BEGIN
     UPDATE graph SET graph.ordering=IF(@activation=1, -1, @max) WHERE graph.id=id;
     UPDATE graph SET graph.ordering=IF(@activation=1, graph.ordering-1, graph.ordering) WHERE graph.ordering > @graphOrder;
     UPDATE graph SET graph.activated=IF(@activation=1, 0, 1) WHERE graph.id=id;
+END$$
+
+CREATE PROCEDURE idb.set_duration(id INTEGER, duration INTEGER)
+BEGIN
+    UPDATE graph SET graph.duration=duration WHERE graph.id=id;
+    UPDATE graph SET graph.start=NULL WHERE graph.id=id;
+    UPDATE graph SET graph.finish=NULL WHERE graph.id=id;
+END$$
+
+CREATE PROCEDURE idb.set_start_and_finish(id INTEGER, start DATETIME(6), finish DATETIME(6))
+BEGIN
+    UPDATE graph SET graph.duration=NULL WHERE graph.id=id;
+    UPDATE graph SET graph.start=start WHERE graph.id=id;
+    UPDATE graph SET graph.finish=finish WHERE graph.id=id;
 END$$
 
 DELIMITER ;
