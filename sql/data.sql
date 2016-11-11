a8b36d3db0087ddefd9d60c609415e1059b43d13
Kim Songhyun <yeonmu4121@gmail.com>
2016-11-11 22:53:29 +0900

    Modify get_data()
    Three get_data procedures for specific conditions, are combined.

--- sql/data.sql
+++ sql/data.sql
@@ -1,7 +1,4 @@
 DROP PROCEDURE IF EXISTS idb.insert_data;
-DROP PROCEDURE IF EXISTS idb.get_data_in_realtime;
-DROP PROCEDURE IF EXISTS idb.get_data_from;
-DROP PROCEDURE IF EXISTS idb.get_data_from_to;
 DROP PROCEDURE IF EXISTS idb.get_data;
 
 DELIMITER $$
@@ -11,52 +8,16 @@ BEGIN
     INSERT INTO idb.data VALUES(label_id, value, updated);
 END$$
 
-CREATE PROCEDURE idb.get_data_in_realtime(graph_id INTEGER)
-BEGIN
-    SELECT label.device_id, label.id, data.value, data.updated
-    FROM label, data, graph
-    WHERE label.id IN (SELECT label_id FROM connects WHERE connects.graph_id=graph_id)
-        AND graph_id=graph.id
-        AND NOW(6) - INTERVAL graph.duration SECOND < data.updated
-	ORDER BY data.updated ASC;
-END$$
-
-CREATE PROCEDURE idb.get_data_from(graph_id INTEGER)
-BEGIN
-    SELECT label.device_id, label.id, data.value, data.updated
-    FROM label, data, graph
-    WHERE label.id IN (SELECT label_id FROM connects WHERE connects.graph_id=graph_id)
-        AND graph_id=graph.id
-		AND graph.start < data.updated
-	ORDER BY data.updated ASC;
-END$$
-
-CREATE PROCEDURE idb.get_data_from_to(graph_id INTEGER)
-BEGIN
-    SELECT label.device_id, label.id, data.value, data.updated
-    FROM label, data, graph
-    WHERE label.id IN (SELECT label_id FROM connects WHERE connects.graph_id=graph_id)
-        AND graph_id=graph.id
-        AND graph.start < data.updated
-        AND graph.finish > data.updated
-	ORDER BY data.updated ASC;
-END$$
-
 CREATE PROCEDURE idb.get_data(graph_id INTEGER)
 BEGIN
-	SET @duration = (SELECT graph.duration FROM graph WHERE graph.id=graph_id);
-    SET @start = (SELECT graph.start FROM graph WHERE graph.id=graph_id);
-    SET @finish = (SELECT graph.finish FROM graph WHERE graph.id=graph_id);
-
-	IF @duration IS NOT NULL THEN
-		CALL get_data_in_realtime(graph_id);
-	ELSEIF @start IS NOT NULL THEN
-		IF @finish IS NOT NULL THEN
-			CALL get_data_from_to(graph_id);
-		ELSE
-			CALL get_data_from(graph_id);
-		END IF;
-	END IF;
+    SELECT label.device_id, label.id, data.value, data.updated
+    FROM label, data, graph
+    WHERE label.id IN (SELECT label_id FROM connects WHERE connects.graph_id=graph_id)
+        AND graph_id=graph.id
+        AND (graph.duration IS NULL OR NOW(6) - INTERVAL graph.duration SECOND < data.updated)
+        AND (graph.start IS NULL OR graph.start < data.updated)
+        AND (graph.finish IS NULL OR graph.finish > data.updated)
+    ORDER BY data.updated ASC;
 END$$
 
 DELIMITER ;
