0099767273dba15ce93fd1546c31fbe634b306d1
Kim Songhyun <yeonmu4121@gmail.com>
2016-11-21 18:40:32 +0900

    Bugfix: data duplication

--- sql/data.sql
+++ sql/data.sql
@@ -13,20 +13,20 @@ BEGIN
     SET @data_count = (SELECT graph.data_count FROM graph WHERE graph.id=graph_id);
     SET @i = 0;
     IF @data_count IS NULL THEN
-        SELECT label.id, data.value, data.updated
-        FROM label, data, graph
-        WHERE label.id IN (SELECT label_id FROM connects WHERE connects.graph_id=graph_id)
-            AND graph_id=graph.id
+        SELECT data.label_id, data.value, data.updated
+        FROM data, graph
+        WHERE data.label_id IN (SELECT label_id FROM connects WHERE connects.graph_id=graph_id)
+            AND graph.id=graph_id
             AND (graph.duration IS NULL OR NOW(6) - INTERVAL graph.duration SECOND < data.updated)
             AND (graph.start IS NULL OR graph.start < data.updated)
             AND (graph.finish IS NULL OR graph.finish > data.updated)
         ORDER BY data.updated ASC;
     ELSE
         SELECT * FROM (
-            SELECT label.id, data.value, data.updated
-            FROM label, data, graph
-            WHERE label.id IN (SELECT label_id FROM connects WHERE connects.graph_id=graph_id)
-                AND graph_id=graph.id
+            SELECT data.label_id, data.value, data.updated
+            FROM data, graph
+            WHERE data.label_id IN (SELECT label_id FROM connects WHERE connects.graph_id=graph_id)
+                AND graph.id=graph_id
                 AND (@i:=@i+1) between 1 and @data_count
             ORDER BY data.updated DESC
         ) AS alias
