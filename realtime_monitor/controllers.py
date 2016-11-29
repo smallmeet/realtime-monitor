9bbb4ea4a335accb2e604c92a81262a073b86cdf
Kim Songhyun <yeonmu4121@gmail.com>
2016-11-29 15:32:24 +0900

    Implement change-name page for graph

--- realtime_monitor/controllers.py
+++ realtime_monitor/controllers.py
@@ -3,17 +3,21 @@ from realtime_monitor.models import Graph
 import realtime_monitor.json as json
 
 graphPages = Blueprint('graph', __name__)
+config = json.loadJSON(open('config.json').readlines())
 
 @graphPages.route('/create')
 def createGraph():
-    graph = Graph(json.loadJSON(open('config.json').readlines()))
+    graph = Graph(config)
     newGraph = graph.create()
     graph.close()
     return json.dict2json(newGraph)
 
 @graphPages.route('/change-name/<int:graphId>/<name>')
 def changeGraphName(graphId, name):
-    return name
+    graph = Graph(config)
+    changedGraph = graph.changeName(graphId, name)
+    graph.close()
+    return json.dict2json(changedGraph)
 
 @graphPages.route('/delete/<int:graphId>')
 def deleteGraph(graphId):
