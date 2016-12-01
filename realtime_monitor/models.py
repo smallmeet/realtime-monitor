6f6c3e9f8e178b4759f2e03be5465ae7910e5d80
Kim Songhyun <yeonmu4121@gmail.com>
2016-12-01 14:39:18 +0900

    Remove returning dictionaries

--- realtime_monitor/models.py
+++ realtime_monitor/models.py
@@ -3,7 +3,6 @@ from pymysql.connections import Connection
 class BaseConn(Connection):
     def __init__(self, config):
         super().__init__(host=config['host'], port=int(config['port']), user=config['user'], passwd=config['passwd'], db=config['db'], charset='utf8')
-        self.autocommit_mode = True
 
 class Graph(BaseConn):
     def __init__(self, config):
@@ -12,22 +11,17 @@ class Graph(BaseConn):
     def create(self):
         cur = self.cursor()
         cur.execute('CALL create_graph()')
-        cur.execute('SELECT graph.id, graph.name FROM graph WHERE graph.id=LAST_INSERT_ID()')
-        row = cur.fetchone()
         self.commit()
         cur.close()
-        return {'id':row[0], 'name':row[1]}
 
     def changeName(self, graphId, name):
         cur = self.cursor()
         cur.execute('CALL change_graph_name({graphId}, \'{name}\')'.format(graphId=graphId, name=name))
         self.commit()
         cur.close()
-        return {'id':graphId, 'name':name}
 
     def delete(self, graphId):
         cur = self.cursor()
         cur.execute('CALL delete_graph({graphId})'.format(graphId=graphId))
         self.commit()
         cur.close()
-        return {'id':graphId}
