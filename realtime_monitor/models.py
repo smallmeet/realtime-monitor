87e787fb2f7657d1c1c2c95f7e622a13320bcf85
Kim Songhyun <yeonmu4121@gmail.com>
2016-12-01 14:45:00 +0900

    Implement Device model

--- realtime_monitor/models.py
+++ realtime_monitor/models.py
@@ -4,6 +4,26 @@ class BaseConn(Connection):
     def __init__(self, config):
         super().__init__(host=config['host'], port=int(config['port']), user=config['user'], passwd=config['passwd'], db=config['db'], charset='utf8')
 
+class Device(BaseConn):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def create(self):
+        cur = self.cursor()
+        cur.execute('CALL create_device()')
+        self.commit()
+        cur.close()
+
+    def changeName(self, deviceId, name):
+        cur = self.cursor()
+        cur.execute('CALL change_device_name({deviceId}, \'{name}\')'.format(deviceId=deviceId, name=name))
+        self.commit()
+        cur.close()
+
+    def delete(self, deviceId):
+        cur = self.cursor()
+        cur.execute('CALL delete_device({deviceId})'.format(deviceId=deviceId))
+
 class Graph(BaseConn):
     def __init__(self, config):
         super().__init__(config)
